5767e1a6a09d200795bd4b9644fef10ce6a3adf5
Mohammed Hijazi <7j.mo7ammed@gmail.com>
2025-12-31 19:38:56 +0300

    More Practice Queries

--- phase_2_joins.sql
+++ phase_2_joins.sql
@@ -19,10 +19,6 @@ FROM accounts a
 JOIN web_events we
 ON we.account_id = a.id
 WHERE a.name LIKE 'Walmart';
-
-SELECT * FROM sales_reps;
-SELECT * FROM region;
-SELECT * FROM accounts;
 -- 3. Provide a table that provides the region for each sales_rep along with their associated accounts. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
 SELECT accounts.name account, sales_reps.name rep,  region.name region
 FROM sales_reps
@@ -40,4 +36,74 @@ JOIN sales_reps s
     ON a.sales_rep_id = s.id
 JOIN region r
     ON s.region_id = r.id;
-
+-- 5. Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
+SELECT a.name account, s.name rep, r.name 
+FROM accounts a
+JOIN sales_reps s
+ON a.sales_rep_id = s.id
+JOIN region r
+ON s.region_id = r.id
+WHERE r.name = 'Midwest'
+ORDER BY a.name; 
+-- 6. Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for accounts where the sales rep has a first name starting with S and in the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
+SELECT a.name account, s.name rep, r.name 
+FROM accounts a
+JOIN sales_reps s
+ON a.sales_rep_id = s.id
+JOIN region r
+ON s.region_id = r.id
+WHERE r.name = 'Midwest' AND s.name LIKE 'S%'
+ORDER BY a.name; 
+-- 7. Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for accounts where the sales rep has a last name starting with K and in the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
+SELECT a.name account, s.name rep, r.name 
+FROM accounts a
+JOIN sales_reps s
+ON a.sales_rep_id = s.id
+JOIN region r
+ON s.region_id = r.id
+WHERE r.name = 'Midwest' AND s.name LIKE '% K%'
+ORDER BY a.name; 
+-- 8. Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. However, you should only provide the results if the standard order quantity exceeds 100. Your final table should have 3 columns: region name, account name, and unit price. In order to avoid a division by zero error, adding .01 to the denominator here is helpful total_amt_usd/(total+0.01).
+SELECT r.name region_name, a.name account_name, o.total_amt_usd/(o.total+0.01) unit_price
+FROM orders o
+JOIN accounts a
+    ON o.account_id = a.id  
+JOIN sales_reps s
+    ON a.sales_rep_id = s.id
+JOIN region r
+    ON s.region_id = r.id
+WHERE o.standard_qty > 100;
+-- 9. Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. However, you should only provide the results if the standard order quantity exceeds 100 and the poster order quantity exceeds 50. Your final table should have 3 columns: region name, account name, and unit price. Sort for the smallest unit price first. In order to avoid a division by zero error, adding .01 to the denominator here is helpful (total_amt_usd/(total+0.01).
+SELECT r.name region_name, a.name account_name, o.total_amt_usd/(o.total+0.01) unit_price
+FROM orders o
+JOIN accounts a
+    ON o.account_id = a.id  
+JOIN sales_reps s
+    ON a.sales_rep_id = s.id
+JOIN region r
+    ON s.region_id = r.id
+WHERE o.standard_qty > 100 AND poster_qty > 50
+ORDER BY unit_price;
+-- 10. Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. However, you should only provide the results if the standard order quantity exceeds 100 and the poster order quantity exceeds 50. Your final table should have 3 columns: region name, account name, and unit price. Sort for the largest unit price first. In order to avoid a division by zero error, adding .01 to the denominator here is helpful (total_amt_usd/(total+0.01).
+SELECT r.name region_name, a.name account_name, o.total_amt_usd/(o.total+0.01) unit_price
+FROM orders o
+JOIN accounts a
+    ON o.account_id = a.id  
+JOIN sales_reps s
+    ON a.sales_rep_id = s.id
+JOIN region r
+    ON s.region_id = r.id
+WHERE o.standard_qty > 100 AND poster_qty > 50
+ORDER BY unit_price DESC;
+-- 11. What are the different channels used by account id 1001? Your final table should have only 2 columns: account name and the different channels. You can try SELECT DISTINCT to narrow down the results to only the unique values.
+SELECT DISTINCT a.name name, w.channel channel
+FROM accounts a
+JOIN web_events w
+    ON a.id = w.account_id
+WHERE a.id = 1001;
+-- 12. Find all the orders that occurred in 2015. Your final table should have 4 columns: occurred_at, account name, order total, and order total_amt_usd.
+SELECT o.occurred_at, a.name, o.total, o.total_amt_usd
+FROM orders o
+LEFT JOIN accounts a
+    ON o.account_id = a.id
+WHERE o.occurred_at BETWEEN '01-01-2015' AND '12-31-2015';
